3aa88c5037bce62b720d828999d100fe5185de56
Jakob Sabatula <j.sabatula@setonhill.edu>
2020-02-07 20:38:39 -0500

    Initial Commit

---
+++ Homework 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework 2
+//
+//  Created by Jakob Sabatula on 2/7/20.
+//  Copyright © 2020 Jakob Sabatula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
